5a061d2a2b1d92af9d50ec196a92faefadb897b0
EJH <margotinjune@gmail.com>
2025-10-16 15:47:00 +0900

    feat: 예매처 정보 업데이트 로직 개선 및 중복 스크래핑 방지
    KOPIS 웹 스크래핑 기반 예매처 정보 업데이트 시스템을 개선하여 중복 작업을 방지
    
    주요 변경 사항:
    - 이미 예매처 데이터가 존재하는 항목 자동 건너뛰기 기능 추가
    - 예매처 데이터가 없는 항목만 조회하는 Repository 쿼리 메서드 추가
    - 전체 업데이트/최근 업데이트 모두 중복 스크래핑 방지 로직 적용
    - 불필요한 웹 스크래핑 작업 제거로 업데이트 시간 단축

--- src/main/java/com/everyplaceinkorea/epik_boot3_api/repository/musical/MusicalRepository.java
+++ src/main/java/com/everyplaceinkorea/epik_boot3_api/repository/musical/MusicalRepository.java
@@ -95,4 +95,7 @@ public interface MusicalRepository extends JpaRepository<Musical, Long> {
 
     List<Musical> findByFacilityIsNotNullAndHallIsNull();
 
+    @Query("SELECT m FROM Musical m WHERE m.kopisId IS NOT NULL " +
+            "AND (m.kopisTicketOffices IS NULL OR m.kopisTicketOffices = '{}' OR m.kopisTicketOffices = '')")
+    Page<Musical> findMusicalsWithoutTicketOffices(Pageable pageable);
 }
